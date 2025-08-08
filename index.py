9af3923300bbffc1da6b6b4baf23f2958b0410a2
missmunazza <missmunazza.01@gmail.com>
2025-08-08 10:15:54 +0400

    first commit

---
+++ index.py
@@ -0,0 +1,74 @@
+print("Hello Python")
+
+p = 5
+print(p)
+
+if 3 > 5:
+    print (True)
+else:
+    print(False)
+
+#Data Types
+# there are 4-5 most important data types of python
+
+integer = 4
+string = ('This is string')
+Float = 5.4
+Charactar = 8
+Double = 5647828993.9366367389
+
+print(type(integer))
+print(type(string))
+print(type(Float))
+print(type(Charactar))
+print(type(Double))
+
+print(type (5))
+
+print(integer, string, Float, Charactar, Double)
+
+print(integer)
+print(string)
+print(Float)
+print(Charactar)
+print(Double)
+
+ab = 5
+cg = "5"
+if ab == cg:
+    print("yes")
+else:
+    print("No")
+
+    z = 2
+    z = 2
+if z == z: 
+     print("yes")
+else:
+     print("No")
+
+
+     #variables
+     #constants
+     #datatypes
+     #comments
+     #syntax
+     #typecasting
+     #variable names
+
+     x = my
+     y = my
+     if x := y:
+        print("yes  , they're equal")
+     else:
+        print("No, they're not eequal")
+
+
+        name = input("Enter your name..")
+        print("welcome", name)
+gi
+        #python slicing
+        #slicing strings
+        #modify strings
+
+        #concatenate string
